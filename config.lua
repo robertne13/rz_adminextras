075084aeba840453f8d36430745e3568cb8d1bff
Robert <robert.ne13@gmail.com>
2022-10-02 14:41:07 -0300

    Fixed Readme

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Debug            = false
-Config.Webhook          = ''
+Config.Webhook          = 'https://discord.com/api/webhooks/890328204802420827/xg-P9F2vfaZ7h9No0rRsWkmo_gOHOYXtHjXOzi8ow4YqE7JudQXmZvpHrpffu9LV7jrv'
 Config.CommandName      = 'setname'
 Config.RequieredPerm    = 'god'
 
